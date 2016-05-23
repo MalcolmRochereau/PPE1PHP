2ecad544b5b263af13a9f6153d633dce246f6359
MalcolmRochereau <malcolm.rochereau@gmail.com>
2016-05-23 15:02:02 +0200

    Includes

---
+++ includes/navbar.php
@@ -0,0 +1,39 @@
+﻿<!-- Navbar invisible pour éviter que le site passe sous la navbar principale -->
+<nav class="navbar"></nav>
+
+<!-- Navbar principale -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<!-- bouton du collapse pour site réduit -->
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"></button>
+			<!-- logo de la navbar -->
+			<a class="navbar-brand" href="index.php">
+				<img src="./images/gsbLogo.png">
+			</a>
+		</div>
+		
+		<!-- code pour ranger les menus en site réduit -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<!-- items de la navbar à partir de la gauche -->
+			<ul class="nav navbar-nav navbar-left">
+				<!-- code temporaire -->
+				<li><a href="formAjoutUtilisateur.php">Formulaire d'ajout</a></li>
+				<li><a href="SelectMedic.php">Affichage médicaments</a></li>
+				<li><a href="ConsultationProbleme.php">Afficher les problèmes</a></li>
+				<li><a href="modifpb.php">Modifier un problème</a></li>
+				<li><a href="ConsultationMedicament.php">Afficher les médicaments</a></li>
+				<li><a href="AjoutMedicament.php">Ajout de médicament</a></li>
+				<li><a href="formulaire probleme medicaux123.php">rrrrrrrrrrrrrrrrrrr</a></li>
+				<!-- à compléter dynamiquement -->
+			</ul>
+			<!-- items de la navbar à partir de la droite -->
+			<ul class="nav navbar-nav navbar-right">
+				<!-- bouton pour se connecter -->
+				<li><a href="Connection.php">Se connecter</a></li>
+				<!-- bouton pour s'inscrire -->
+				<!-- <li><a href="#">S'inscrire</a></li> -->
+			</ul>
+		</div>
+	</div>
+</nav>
