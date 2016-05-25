2e537dcc557f017d232d579afeb465aaac142658
magnoob <rom-tennis@hotmail.fr>
2016-05-25 14:57:09 +0200

    modification du css et du form saisi pb

--- formPbMedicaux.php
+++ formPbMedicaux.php
@@ -1,20 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<title>jQuery UI Datepicker - Default functionality</title>
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-<!--<link rel="stylesheet" href="css/calendrier.css" type="text/css" />-->
-<script type="text/javascript" src="js/jquery-2.1.3.js"></script> 
-<script type="text/javascript" src="js/jquery-ui.js"></script>
+    <meta charset="utf-8" >
+    <link rel="stylesheet" href="css\bootstrap.css" >
+    <link rel="stylesheet" href="css\style.css" >
+    <title>Formulaire</title>
+    <script src="js\jquery.js"></script>
+    <script src="js\bootstrap.js"></script>
+    <script src="js\fonctions.js"></script>
 	<script>
 		$(function() {
 			$( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
 		});
 	</script>
-	<?php include("includes/AccesBDD.php"); ?>
 </head>
 <body>
+    <?php 
+    include("includes/navbar.php");
+    include("includes/accesBDD.php");
+    $sessiontempo = 0;
+    ?>
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10">
+        <div class="milieu">
+            <center>
+                <div class="titreForm">Ajout d'un problème médical</div>
+                <img src="images/fondTitre.png" alt="" class="imageTitre">
+                <div class="row">
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-10">
+
 	<?php
 		if(count($_POST)>0)
 		{
@@ -38,11 +53,7 @@
 			}
 		}
     ?>
-	<h3>Saisie des problemes</h3>
-	<br><br>
-	<form action="" method="POST">
-		<fieldset>
-			<legend><span class="saisieprob"><b>Saisie des probleme</b></span></legend>
+	
 			<p>Veuillez nous préciser la date de la survenue du problemes :<input type="text" name="datepicker" id="datepicker"></p>
 			<br>Entrer un commentaire <br>
 			<textarea id="commentaire" name="commentaire" cols="40" rows="5" /></textarea>
@@ -76,6 +87,11 @@
 		</fieldset>
 		<br><br><br><input type="submit" name="nom" value="Valider" >
 	</form>
-	
+</div>
+                </div> 
+            </center>
+        </div>
+    </div>
+    <div class="col-sm-1"></div>
 </body>
 </html>
