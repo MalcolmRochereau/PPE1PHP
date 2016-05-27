68996ebed750272c189b23d56299f793a40387f7
GregoryCamps <gregorycamps.jr@gmail.com>
2016-05-27 09:51:48 +0200

    Connexion opérationelle

--- connexion.php
+++ connexion.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -13,7 +16,6 @@
     <?php 
     include("includes/navbar.php");
     include("includes/accesBDD.php");
-    $sessiontempo = 0;
     ?>
     <div class="col-sm-1"></div>
     <div class="col-sm-10">
@@ -24,29 +26,41 @@
                 <div class="row">
                     <div class="col-sm-1"></div>
                     <div class="col-sm-10"><h2>
-<?php
-	if (!isset($_POST['pseudo'])) //On est dans la page de formulaire
-	{
-		?>
-		<form method="post" action="connexion.php">
-			<fieldset>
-				Connexion
-				<p>
-					<label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
-					<label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
-				</p>
-			</fieldset>
-			<p><input type="submit" value="Connexion" /></p>
-		</form>
-		<a href="./register.php">Pas encore inscrit ?</a>
-		<?php
-	}
-?>
-                    </h2></div>
-                </div> 
-            </center>
-        </div>
-    </div>
-    <div class="col-sm-1"></div>
+                        <?php
+                        if (!isset($_POST['pseudo'])) //On est dans la page de formulaire
+                        {
+                            ?>
+                            <form method="post" action="connexion.php">
+                                <fieldset>
+                                    Connexion
+                                    <p>
+                                       <label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
+                                       <label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
+                                   </p>
+                               </fieldset>
+                               <p><input type="submit" value="Connexion" /></p>
+                           </form>
+                           <a onclick="alert('Dommage !')">Pas encore inscrit ?</a>
+                           <?php
+                       }
+                       else
+                       {
+                            $req = $bdd->query('SELECT * FROM v_utilisateurs WHERE identifiant = "'.$_POST['pseudo'].'"');
+                            $ligne = $req->fetch();
+                            if(sha1($_POST['password'] == $ligne['mdp']))
+                            {
+                                $_SESSION['role'] = $ligne['role'];
+                                $_SESSION['id'] = $ligne['id'];
+                                header('Location: index.php');
+                                exit();
+                            }
+                       }
+                       ?>
+                   </h2></div>
+               </div> 
+           </center>
+       </div>
+   </div>
+   <div class="col-sm-1"></div>
 </body>
 </html>
